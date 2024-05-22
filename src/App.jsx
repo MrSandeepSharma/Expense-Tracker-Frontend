88d97eefa691f8d288294ad7f6a1947d580fecb7
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 12:15:50 +0530

    current user check added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from 'react'
 import { Outlet } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+
+import { Loader } from './components'
+import { auth } from "./auth"
+import { login } from './store/authSlice'
 
 import './App.css'
 
 function App() {
-  return (
-    <Outlet />
-  )
+
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  useEffect(()=> {
+    setLoading(true)
+    auth.getCurrentUser()
+      .then(user => {
+        if(user) {
+          const newData = {
+            username: user.username,
+            email: user.email
+          }
+          dispatch(login(newData))
+          setLoading(false)
+        }
+      })
+  },[])
+
+  return loading ? <Loader /> : <Outlet />
 }
 
-export default App
+export default App;
