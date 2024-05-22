a450221953f1ad45b4122f0620edac16b885e320
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 11:44:00 +0530

    loader and toastmsg component created

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+import loaderAnim from "../assets/loader.gif"
+
+function Loader() {
+  return (
+    <div 
+        className="absolute w-full min-h-screen left-0 top-0 flex justify-center items-center z-30"
+    >
+      <img src={loaderAnim} alt="loading" />
+    </div>
+  )
+}
+
+export default Loader;
---
+++ src/components/Toastmsg.jsx
@@ -0,0 +1,9 @@
+import { ToastContainer } from 'react-toastify'
+
+import 'react-toastify/dist/ReactToastify.css';
+
+function Toastmsg() {
+  return <ToastContainer />
+}
+
+export default Toastmsg;
