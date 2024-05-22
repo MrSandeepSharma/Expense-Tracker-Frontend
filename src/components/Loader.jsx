81fbbe9ea18b47ce0ac98c8301cfb233ec5e1c06
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 13:50:59 +0530

    css fix

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,7 +3,7 @@ import loaderAnim from "../assets/loader.gif"
 function Loader() {
   return (
     <div 
-        className="bg-zinc-100 absolute w-full min-h-screen left-0 top-0 flex justify-center items-center z-30"
+        className="absolute w-full min-h-screen left-0 top-0 flex justify-center items-center z-30"
     >
       <img src={loaderAnim} alt="loading" />
     </div>
