505a4719daa7f7debf8cd64d5f2e6aebe32aa47f
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 12:25:51 +0530

    logout functinality added to homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,10 +1,31 @@
-import { Header, Footer } from "../components";
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+
+import { Header, Footer, Loader } from "../components";
+import { auth } from "../auth"
+import { logout } from "../store/authSlice" 
 
 function Homepage() {
+
+  const [loading, setLoading] = useState(false)
+  const dispatch = useDispatch()
+
+  async function logoutCurrentUser() {
+    setLoading(true)
+    const res = await auth.logoutUser()
+    if (res) {
+      dispatch(logout())
+      setLoading(false)
+    }
+  }
+
   return (
     <>
-      <Header btnType="button" btnText="Logout" />
-      <main id="main">
+      <Header btnType="button" btnText="Logout" onClick={logoutCurrentUser} />
+      <main id="main" className="bg-zinc-100 py-4 px-6 sm:py-6 sm:px-8 md:py-8 md:px-16">
+        {
+          loading && <Loader />
+        }
         <h1>Home Page</h1>
       </main>
       <Footer />
