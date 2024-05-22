ea18147c36828ad12b40d3836d4b75ae0d5575fa
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 12:27:22 +0530

    few css fix

--- src/pages/Errorpage.jsx
+++ src/pages/Errorpage.jsx
@@ -4,7 +4,7 @@ import { FaChevronLeft } from "react-icons/fa";
 
 function Errorpage() {
   return (
-    <div className="px-6 flex flex-col justify-center items-center gap-8 min-h-screen">
+    <div className="bg-zinc-100 px-6 flex flex-col justify-center items-center gap-8 min-h-screen">
       <h1 className="text-6xl">404</h1>
       <p className="max-w-sm text-center sm:text-xl">
         Oops! The page you're looking for isn't here. Why not head back to our 
