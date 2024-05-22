2468caedff890dc57f4f619806f677e7a9148713
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 09:47:24 +0530

    Error page completed

--- src/pages/Errorpage.jsx
+++ src/pages/Errorpage.jsx
@@ -1,8 +1,17 @@
+import { PrimaryBtn } from "../components/Button";
+
+import { FaChevronLeft } from "react-icons/fa";
+
 function Errorpage() {
   return (
-    <main id="main">
-        <h1>Error Page</h1>
-    </main>
+    <div className="px-6 flex flex-col justify-center items-center gap-8 min-h-screen">
+      <h1 className="text-6xl">404</h1>
+      <p className="max-w-sm text-center sm:text-xl">
+        Oops! The page you're looking for isn't here. Why not head back to our 
+        homepage and explore from there?
+      </p>
+      <PrimaryBtn icon={<FaChevronLeft />} path="/" text="Go to Homepage" />
+    </div>
   )
 }
 
