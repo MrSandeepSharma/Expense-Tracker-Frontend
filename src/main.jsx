ef9b39ec9bc3644497425a0276dd926ebf150b92
Sandeep <sandipsharma2445@gmail.com>
2024-05-22 11:57:21 +0530

    redux installed and store configured

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './App.jsx'
 import { Loginpage, Signuppage, RouteController, Errorpage } from "./pages"
+import store from './store/store.js'
 
 import './index.css'
 
@@ -29,6 +31,8 @@ const router = createBrowserRouter([{
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store}>
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>,
 )
